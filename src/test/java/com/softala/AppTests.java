b2781c0f9937fba955fc26bf3303c822c79985c3
Somac <markus.ylinenn@gmail.com>
2015-05-14 13:45:06 +0300

    Second commit

---
+++ src/test/java/com/softala/AppTests.java
@@ -0,0 +1,22 @@
+package com.softala;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+
+public class AppTests {
+
+    //Placeholder luokka testeille (jos sellaisia tulee..).
+	
+	@Test
+	public void testitesti() {
+		System.out.println("moi");
+	}
+
+}
